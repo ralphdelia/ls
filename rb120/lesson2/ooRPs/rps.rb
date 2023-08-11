4e93865732d6849c622f130bf9a253ed5685726d
Ralph Delia <Ralphdelia1@gmail.com>
2023-08-11 14:08:30 -0400

    oorps working well

--- rb120/lesson2/ooRPs/rps.rb
+++ rb120/lesson2/ooRPs/rps.rb
@@ -1,4 +1,6 @@
-require 'pry'
+require 'yaml'
+MESSAGES = YAML.load_file('rps_messages.yml')
+
 class Move
   VALUES = %w(rock paper scissors spock lizard).freeze
   WIN_INDEX = { 'rock' => ['lizard', 'scissors'],
@@ -9,12 +11,16 @@ class Move
 
   attr_reader :value
 
+  def initialize(value)
+    @value = value
+  end
+
   def <=>(other)
-    if WIN_INDEX[@value].include?(other.value)
+    if WIN_INDEX[value].include?(other)
       1
-    elsif WIN_INDEX[other.value].include?(@value)
+    elsif WIN_INDEX[other].include?(value)
       -1
-    else 
+    else
       0
     end
   end
@@ -24,42 +30,15 @@ class Move
   end
 end
 
-class Scissors < Move
-  def initialize
-    @value = 'scissors'
-  end
-
-end
-
-class Rock < Move
-  def initialize
-    @value = 'rock'
-  end
-end
-
-class Paper < Move
-  def initialize
-    @value = 'paper'
-  end
-end
-
-class Spock < Move
-  def initialize
-    @value = 'spock'
-  end
-end
-
-class Lizard < Move
-  def initialize
-    @value = 'lizard'
-  end
-end
-
 class Player
   attr_accessor :move, :name
 
   def initialize
     set_name
+  end
+
+  def string_state
+    "#{name} chose #{move}"
   end
 end
 
@@ -85,22 +64,7 @@ class Human < Player
 
       puts 'Sorry, invalid choice.'
     end
-    self.move = return_collaborator(choice)
-  end
-
-  def return_collaborator(choice)
-    case choice
-    when 'scissors'
-      Scissors.new
-    when 'rock'
-      Rock.new
-    when 'paper'
-      Paper.new 
-    when 'lizard'
-      Lizard.new
-    when 'spock'
-      Spock.new
-    end
+    self.move = Move.new(choice)
   end
 end
 
@@ -110,42 +74,127 @@ class Computer < Player
   end
 
   def choose
-    self.move = [Scissors.new, Paper.new, Rock.new, Spock.new, Lizard.new].sample
+    self.move = Move::VALUES.sample
   end
 end
 
-class RPSGame
-  attr_accessor :human, :computer, :score, :round_winner
+class History
+  attr_accessor :record, :turn
 
   def initialize
-    @human = Human.new
-    @computer = Computer.new
-    @score = Score.new(player_keys)
+    @turn = 1
+    @record = {}
   end
 
-  def display_welcome_message
-    puts 'Welcome to RPS'
+  def file(player, computer, winner, score)
+    time = Time.new
+    record[turn] = { time: "#{time.hour}:#{time.min}",
+                     moves: [player, computer],
+                     winner: winner,
+                     score: score.dup }
+    self.turn += 1
+  end
+
+  def display
+    puts "Game History:"
+    record.each do |turn, data|
+      puts "Turn #{turn}:"
+      puts "  Time: #{data[:time]}"
+      puts "  Player Move: #{data[:moves][0]}"
+      puts "  Computer Move: #{data[:moves][1]}"
+      puts "  Winner: #{data[:winner]}"
+      puts "  Score: #{data[:score]}"
+      puts
+    end
+  end
+end
+
+module Display
+  def display_rules
+    MESSAGES['rules'].each do |r|
+      sleep(0.05)
+      puts r.center(53)
+    end
+  end
+
+  def display_turn
+    display_moves
+    display_winner
+    score.display
+  end
+
+  def display_match_results
+    puts "#{score.leader} has won the match!" if winner?
+  end
+
+  def show_rules?
+    answer = ''
+    loop do
+      puts 'Do you need a tour of the rules? (y/n)'.center(53)
+      answer = gets.chomp
+
+      break if %(y n).include?(answer)
+      puts 'Invalid input please use (y/n)'
+    end
+
+    true if answer == 'y'
+  end
+
+  def display_welcome
+    system 'clear'
+    puts "Hello #{human.name}!".center(53)
+    MESSAGES['welcome'].each { |w| puts w.center(53) }
+
+    display_rules if show_rules?
   end
 
   def display_goodbye_message
     puts 'Thanks for playing!'
   end
 
-  def player_keys
-    [human.name, computer.name]
+  def countdown_shoot
+    MESSAGES['shoot'].each do |m|
+      sleep(0.5)
+      puts m.center(53)
+    end
+  end
+
+  def spacer
+    puts "~" * 53
   end
 
   def display_moves
-    puts "#{human.name} chose #{human.move}"
-    puts "#{computer.name} chose #{computer.move}"
+    countdown_shoot
+    spacer
+    puts format(MESSAGES['moves'], name: "You", move: human.move).center(53)
+    puts format(MESSAGES['moves'], name: computer.name,
+                                   move: computer.move).center(53)
+    spacer
   end
 
   def display_winner
     if round_winner == 'tie'
-      puts "Its a tie"
+      puts "Its a tie".center(53)
     else
-      puts "#{round_winner} won!"
+      puts "#{round_winner} won!".center(53)
     end
+  end
+end
+
+class RPSGame
+  include Display
+
+  attr_accessor :human, :computer, :score, :round_winner, :record
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+    @score = Score.new(player_names)
+    @record = History.new
+  end
+
+  def player_names
+    [human.name, computer.name]
   end
 
   def determine_winner
@@ -177,32 +226,56 @@ class RPSGame
     return false if answer.downcase == 'y'
   end
 
-  def display_match_results
-    puts "#{score.leader} has won the match!" if winner?
+  def game_history?
+    answer = ''
+    loop do
+      puts 'Do you want to see a record of the game history? (y/n)'
+      answer = gets.chomp
+      break if %w(y n).include?(answer.downcase)
+      puts "Invalid input use (y/n)"
+    end
+    record.display if answer.downcase == 'y'
   end
 
-  def play
-    display_welcome_message
+  def display_turn
+    display_moves
+    display_winner
+    score.display
+  end
+
+  def update_turn
+    determine_winner
+    score.update(round_winner)
+    record.file(human.string_state,
+                computer.string_state,
+                round_winner,
+                score.player_points)
+  end
+
+  def turn
     loop do
       human.choose
       computer.choose
-      display_moves
-      determine_winner
-      display_winner
 
-      score.update(round_winner)
-      score.display
-
+      update_turn
+      display_turn
       break if winner? || stop_playing?
     end
+  end
+
+  def play
+    display_welcome
+
+    turn
 
     display_match_results
+    game_history?
     display_goodbye_message
   end
 end
 
 class Score
-  TO_WIN = 3
+  TO_WIN = 5
 
   attr_accessor :player_points
   attr_reader :players
@@ -231,11 +304,19 @@ class Score
     player_points[winner] += 1
   end
 
+  def blank_line
+    puts ''
+  end
+
   def display
-    puts '~Score~'
+    blank_line
+    puts '______________Score______________'.center(53)
+    blank_line
     player_points.each do |k, v|
-      puts "#{k}: #{v}"
+      puts "#{k}: #{v}".center(53)
     end
+    puts '_________________________________'.center(53)
+    blank_line
   end
 end
 
