a5c442c4a69c8d7267429d543050576ae0ea0526
Ralph Delia <Ralphdelia1@gmail.com>
2023-11-12 12:50:08 -0500

    18

--- rb175/sinatra_todos/todo.rb
+++ rb175/sinatra_todos/todo.rb
@@ -82,3 +82,14 @@ post "/lists/:id" do
     redirect "/lists/#{id}"
   end
 end
+
+post "/lists/:id/destroy" do
+  id = params[:id].to_i
+  session[:lists].delete_at(id)
+  
+  session[:success] = "The list has been deleted"
+  redirect "/lists"
+end
+
+post "/list/:id/todos" do
+  
