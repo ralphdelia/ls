ec38b6f316a03b5b76d66acf8b3c0f7861995e41
Ralph Delia <Ralphdelia1@gmail.com>
2023-11-12 16:05:52 -0500

    -20

--- rb175/sinatra_todos/todo.rb
+++ rb175/sinatra_todos/todo.rb
@@ -113,11 +113,12 @@ post "/lists/:list_id/todos" do
   end
 end
 
-post '/list/:list_id/todos/:todo_id/delete' do 
-  @list_id = params[:list_id]
-  @todo_id = params[:todo_id]
+post '/lists/:list_id/todos/:todo_id/destroy' do 
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  todo_id = params[:todo_id].to_i
   
-
-
-  redirect "/lists/#{list_id}"
+  @list[:todos].delete_at(todo_id)
+  session[:success] = "The todo has been deleted."
+  redirect "/lists/#{@list_id}"
 end
